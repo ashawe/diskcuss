43e680874c304cb96ed5f820d02e821c00bc063b
ashawe <harshsaglani@gmail.com>
2023-11-09 23:30:01 -0500

    Add DB integration. Create Message, Topic Tables

--- base/views.py
+++ base/views.py
@@ -1,31 +1,16 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    { 
-        'id' : 1,
-        'name' : 'Dungeons & Dragons'
-    },
-    {
-        'id' : 2,
-        'name' : 'Monopoly'
-    },
-    {
-        'id' : 3,
-        'name' : 'Warhammer'
-    }
-]
+rooms = Room.objects.all()
 
 def home(request):
+    
     context = {'rooms' : rooms}
     return render(request,'base/home.html', context)
 
 def room(request,pk):
-    selectedRoom = None
-    for room in rooms:
-        if room['id'] == int(pk):
-            selectedRoom = room
-            break
+    selectedRoom = Room.objects.get(id=pk)
     context = {'room' : selectedRoom}
     return render(request,'base/room.html',context)
