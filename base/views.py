b3d5f0037080503340f589acde32dc50f1c75880
ashawe <harshsaglani@gmail.com>
2023-11-11 16:53:13 -0500

    Implement Register

--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -14,7 +15,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == "POST":
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -28,12 +29,27 @@ def loginPage(request):
         except:
             messages.error(request, 'Username or Password incorrect')
 
-    context = {}
+    context = {'page':'login'}
     return render(request,'base/login_register.html',context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Error while registering. Please check all fields and try again.')
+    context = {'page':'register', 'form':form}
+    return render(request, 'base/login_register.html',context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
