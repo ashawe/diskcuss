b44d6fb2b8ec9e0f94bd5d90d56adaaf594fa79b
ashawe <harshsaglani@gmail.com>
2023-11-11 16:05:39 -0500

    search field

--- base/views.py
+++ base/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
+def loginPage(request):
+    
+
+    context = {}
+    return render(request,'base/login_register.html',context)
+
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+    
+    room_count = rooms.count()
+
     topics = Topic.objects.all()
-    context = {'rooms' : rooms, 'topics':topics}
+    
+    context = {'rooms' : rooms, 'topics':topics, 'room_count':room_count}
     return render(request,'base/home.html', context)
 
 def room(request,pk):
