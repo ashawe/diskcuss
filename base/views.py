64c90e771f0e21a8ae4fcc30948eebbab5affe81
ashawe <harshsaglani@gmail.com>
2023-11-15 16:31:51 -0500

    Add more templating

--- base/views.py
+++ base/views.py
@@ -97,32 +97,42 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context  = {'form':form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room = Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description')
+        )
+        room.participants.add(request.user)
+        room.save()
+        return redirect('home')
+    context  = {'form':form, 'page':'Create', 'topics':topics}
     return render(request,'base/room_form.html', context)
 
 @login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         messages.error(request,'Bad Request')
         return redirect('home')
 
     if request.method == "POST":
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context = {'form':form}
+    context  = {'form':form, 'page':'Update', 'topics':topics, 'room_topic':room.topic.name}
     return render(request,'base/room_form.html',context)
 
 @login_required(login_url='login')
@@ -151,3 +161,8 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('room',pk=roomid)
     return render(request, 'base/delete.html',{'obj':message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    context = {}
+    return render(request,'base/update-user.html', context)
